5c21e078d2cf47fff5f5fdab85383a808dcc1990
William Harmer <willjharmer@gmail.com>
2015-06-11 12:43:44 +0100

    wip

--- tests/features/config_steps.py
+++ tests/features/config_steps.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
 from lettuce import step, world
+from nose.tools import *
 
 @step('Given I have a <config> config object')
 def given_i_have_a_config_config_object(step, config):
     world.config = config
 
-@step(u'When I init app with <config> object')
+@step(u'When I init app with the object')
 def when_i_init_app_with_config_object(step):
-    assert False, 'This step must be implemented'
+    world.app.config = world.config
 
 @step(u'Then I have <debug> DEBUG and <testing> TESTING env variables')
 def then_i_have_debug_debug_and_testing_testing_env_variables(step):
-    assert False, 'This step must be implemented'
+    eq_('pass', 'This step must be implemented')
 
 
 
